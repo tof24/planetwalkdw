2ccfc6ff68642efcbca620fcb34c4c9838bf81bc
André Domingues <85996566+tof24@users.noreply.github.com>
2023-01-18 00:31:42 +0000

    trying to upload

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 import Home from './Pages/Home.js'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 import Detail from "./Pages/Detail";
 
 function App() {
 
 
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home ></Home>} />
-          <Route path="/detail" element={<Detail></Detail>} />
-        </Routes>
-      </Router>
-
+      <Home></Home>
   );
 }
 
